22b4747df638cf4be1cfe03d2417a366f63288d9
Rammah Ali <nydoerd@gmail.com>
2021-05-16 02:55:52 +0300

    add validation to signup

--- src/Home/Controllers/createAccount.java
+++ src/Home/Controllers/createAccount.java
@@ -14,6 +14,7 @@ import javafx.stage.FileChooser;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.ResultSet;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -47,14 +48,32 @@ public class createAccount implements Initializable {
         App.navigateTo("login");
     }
 
-
+    private boolean isValidEmailAddress(String email) {
+        String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+        java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+        java.util.regex.Matcher m = p.matcher(email);
+        return m.matches();
+    }
     @FXML
     private void onCreateAccountClick() {
-        // TODO: change country to country code, not 1
-        String query = String.format("INSERT INTO app_user(name, email, password_hash, country_id, picture) VALUES ('%s', '%s', '%s', %d, '%s')",
-                name.getText(), email.getText(), getHashedPassword(password.getText()), 1, profileImagePath);
-           execute(query);
-           App.showSuccessMessage("Account has been created","You are now logged in");
+        // validation
+        if(name.getText().length()<4)
+            App.showError("Name cannot be less than 4 chars","please update your name");
+         else if(!isValidEmailAddress(email.getText()))
+            App.showError("InValid email address","please enter a Valid email");
+        else if(password.getText().length()<5)
+            App.showError("Password cannot be less than 5 chars","please update your password");
+        else if(countryList.getValue().toString().equals("Select country"))
+            App.showError("Country must be selected","please select your country");
+
+        else{
+            // TODO: change country to country code, not 1
+            String query = String.format("INSERT INTO app_user(name, email, password_hash, country_id, picture) VALUES ('%s', '%s', '%s', %d, '%s')",
+                    name.getText(), email.getText(), getHashedPassword(password.getText()), 1, profileImagePath);
+            execute(query);
+            App.showSuccessMessage("Account has been created","You are now logged in");
+        }
+
     }
     @FXML private void selectProfileImage(){
         FileChooser fileChooser = new FileChooser();
