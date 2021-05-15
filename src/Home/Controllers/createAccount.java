302c27f9150fa60dc343c85ea6db59f5b07aacbe
Rammah Ali <nydoerd@gmail.com>
2021-05-15 03:45:18 +0300

    init commit

---
+++ src/Home/Controllers/createAccount.java
@@ -0,0 +1,10 @@
+package Home.Controllers;
+
+public class createAccount {
+
+    private void create_account(){
+
+    }
+
+    private void login(){}
+}
---
+++ src/Home/Controllers/login.java
@@ -0,0 +1,19 @@
+package Home.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+public class login implements Initializable {
+    @FXML
+    private TextField email;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Button go;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+}
