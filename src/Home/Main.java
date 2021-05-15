5b28b5564da805ab65b0320fb82cd30f6214c856
Rammah Ali <nydoerd@gmail.com>
2021-05-15 05:32:29 +0300

    add App class

--- src/Home/Main.java
+++ src/Home/Main.java
@@ -1,35 +1,9 @@
+
 package Home;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-
-public class Main extends Application {
-  double x,y;
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLS/login.fxml"));
-        primaryStage.initStyle(StageStyle.UNDECORATED);
-        root.setOnMousePressed(mouseEvent -> {
-            x = mouseEvent.getSceneX();
-            y= mouseEvent.getSceneY();
-        });
-        root.setOnMouseDragged(mouseEvent -> {
-           primaryStage.setX(mouseEvent.getScreenX()-x);
-           primaryStage.setY(mouseEvent.getScreenY()-y);
-        });
-        primaryStage.setTitle("Musiconnect");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
-    }
-
-
+public class Main  {
 
     public static void main(String[] args) {
-
-        launch(args);
+        App.main(args);
     }
 }
