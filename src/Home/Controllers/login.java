b1a914557598dbf3472b41d9fde8d4896a823111
Ali Elbashir <alielbashiir@gmail.com>
2021-05-15 10:15:42 +0300

    Add login handling function

--- src/Home/Controllers/login.java
+++ src/Home/Controllers/login.java
@@ -3,20 +3,52 @@ package Home.Controllers;
 import Home.App;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+
+import static Home.Helper.executeQuery;
+import static Home.Helper.getHashedPassword;
+
 public class login implements Initializable {
-    @FXML private TextField email;
-    @FXML private PasswordField password;
-    @FXML private Button go;
+    @FXML
+    private TextField email;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Button go;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
     }
 
-    @FXML private void onCreateAccountMenuClick() throws IOException {
+    @FXML
+    private void onCreateAccountMenuClick() throws IOException {
         App.navigateTo("createAccount");
+    }
+
+    @FXML
+    private void onLoginClick() throws SQLException {
+        // FIXME: query not working
+        String query = String.format("SELECT password_hash FROM app_user WHERE email = '%s'", email.getText());
+        ResultSet resultSet = executeQuery(query);
+        String email = resultSet.getString("email");
+        String passwordHash = resultSet.getString("password_hash");
+
+        if (email == null) {
+            System.out.println("Incorrect username or password");
+            return;
+        }
+        if (!getHashedPassword(password.getText()).equals(passwordHash)) {
+            System.out.println("Incorrect username or password");
+        } else {
+            System.out.println("Successful login");
+        }
     }
 }
