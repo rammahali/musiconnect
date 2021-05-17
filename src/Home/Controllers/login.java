db8abacf970db1c22d1603b152e6e61cee5b0c3b
Ali Elbashir <alielbashiir@gmail.com>
2021-05-17 17:03:57 +0300

    Fix login despite wrong password

--- src/Home/Controllers/login.java
+++ src/Home/Controllers/login.java
@@ -2,10 +2,7 @@ package Home.Controllers;
 
 import Home.App;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
@@ -16,7 +13,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
-import static Home.App.loaderFactory;
 import static Home.Helper.executeQuery;
 import static Home.Helper.getHashedPassword;
 
@@ -42,7 +38,7 @@ public class login implements Initializable {
     }
 
     @FXML
-    private void onCloseAppClick()  {
+    private void onCloseAppClick() {
         App.close();
     }
 
@@ -62,17 +58,17 @@ public class login implements Initializable {
             if (!getHashedPassword(password.getText()).equals(passwordHash)) {
                 App.showError("Incorrect email or password", "please try again");
             } else {
+                App.setUserEmail(email.getText());
+                try {
+                    App.navigateTo("userPlaylists");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 App.showSuccessMessage("Successful login", "You are now logged in");
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
-        App.setUserEmail(email.getText());
-        try {
-            App.navigateTo("userPlaylists");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
