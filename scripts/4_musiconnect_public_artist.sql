b176508e1d940a5244e28ede8f67bf3790987fd8
Ali Elbashir <alielbashiir@gmail.com>
2021-05-18 09:46:03 +0300

    Remove ID from SQL inserts

--- scripts/4_musiconnect_public_artist.sql
+++ scripts/4_musiconnect_public_artist.sql
@@ -1,7 +1,7 @@
 \c musiconnect
 
-INSERT INTO public.artist (id, name, country_id, picture) VALUES (1, 'Mozart', 40, 'Mozart.png');
-INSERT INTO public.artist (id, name, country_id, picture) VALUES (2, 'Michael Jackson', 840, 'Michael Jackson.png');
-INSERT INTO public.artist (id, name, country_id, picture) VALUES (4, 'Adele', 826, 'Adele.png');
-INSERT INTO public.artist (id, name, country_id, picture) VALUES (3, 'Billie Holiday', 840, 'Billie Holiday.png');
-INSERT INTO public.artist (id, name, country_id, picture) VALUES (5, 'Aretha Franklin', 840, 'Aretha Franklin.png');
+INSERT INTO public.artist (name, country_id, picture) VALUES ('Mozart', 40, 'Mozart.png');
+INSERT INTO public.artist (name, country_id, picture) VALUES ('Michael Jackson', 840, 'Michael Jackson.png');
+INSERT INTO public.artist (name, country_id, picture) VALUES ('Billie Holiday', 840, 'Billie Holiday.png');
+INSERT INTO public.artist (name, country_id, picture) VALUES ('Adele', 826, 'Adele.png');
+INSERT INTO public.artist (name, country_id, picture) VALUES ('Aretha Franklin', 840, 'Aretha Franklin.png');
