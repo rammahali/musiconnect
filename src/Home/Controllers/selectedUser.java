cdd4ad1ac572ae49b0e5080d57ef28a2c2245c36
Ali Elbashir <alielbashiir@gmail.com>
2021-05-19 23:27:49 +0300

    Add copy all from playlist

--- src/Home/Controllers/selectedUser.java
+++ src/Home/Controllers/selectedUser.java
@@ -2,7 +2,6 @@ package Home.Controllers;
 
 import Home.App;
 import Home.Helper;
-import Home.Modules.Song;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -10,7 +9,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
@@ -48,28 +46,33 @@ public class selectedUser implements Initializable {
     TextField selectedUser;
     int user_id;
     ObservableList<String> songs = FXCollections.observableArrayList();
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        Helper.getUserData(displayName,profilePicture);
-        Helper.populateUserNavigator(navigator,"userAlbums");
+        Helper.getUserData(displayName, profilePicture);
+        Helper.populateUserNavigator(navigator, "userAlbums");
 
     }
 
 
-    @FXML private void onSongActionClick(){
+    @FXML
+    private void onSongActionClick() {
 //        String song = songsListView.getSelectionModel().getSelectedItem();
 //        if(song!=null){
 //            selectedSong.setText(song);
 //        }
     }
 
-    @FXML private void viewSong() throws IOException, SQLException {
+    @FXML
+    private void viewSong() throws IOException, SQLException {
 //         if(songs.contains(selectedSong.getText())){
 //             App.setSongID(getSongID(selectedSong.getText()));
 //             App.navigateTo("selectedSong");
 //         }
     }
-    @FXML private void addSong() throws SQLException {
+
+    @FXML
+    private void addSong() throws SQLException {
 //        String albumName = this.albumName.getText();
 //         int user_id = getUserID(App.getUserEmail());
 //         int song_id= getSongID(selectedSong.getText());
@@ -97,26 +100,26 @@ public class selectedUser implements Initializable {
     }
 
 
-
-    @FXML public void instantiate(int userID){
-        user_id=userID;
+    @FXML
+    public void instantiate(int userID) {
+        user_id = userID;
         String query = "SELECT  * FROM app_user WHERE id =? ORDER BY name ";
         try (PreparedStatement statement = App.connection.prepareStatement(query)) {
             statement.setInt(1, userID);
             ResultSet resultSet = executeQuery(statement);
             resultSet.next();
-                String name = resultSet.getString("name");
-                userName.setText(name);
-               try {
-                   File imageFile = new File(resultSet.getString("picture"));
-                   String imageLocation = imageFile.toURI().toString();
-                   Image pic = new Image(imageLocation, false);
-                   userPicture.setFill(new ImagePattern(pic));
-               }
-               catch (Exception ignored){}
-               populatePopChart();
-               populateClassicChart();
-               populateJazzChart();
+            String name = resultSet.getString("name");
+            userName.setText(name);
+            try {
+                File imageFile = new File(resultSet.getString("picture"));
+                String imageLocation = imageFile.toURI().toString();
+                Image pic = new Image(imageLocation, false);
+                userPicture.setFill(new ImagePattern(pic));
+            } catch (Exception ignored) {
+            }
+            populatePopChart();
+            populateClassicChart();
+            populateJazzChart();
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
@@ -149,7 +152,7 @@ public class selectedUser implements Initializable {
         return resultSet.getInt("category_id");
     }
 
-    private int getPlayList(int userID , int category_id) throws SQLException {
+    private int getPlayList(int userID, int category_id) throws SQLException {
         String query = "SELECT * FROM playlist WHERE user_id = ? AND category_id=?";
         PreparedStatement statement = App.connection.prepareStatement(query);
         statement.setInt(1, userID);
@@ -238,7 +241,7 @@ public class selectedUser implements Initializable {
         idStatement.setInt(2, user_id);
         ResultSet idResult = executeQuery(idStatement);
         if (!idResult.next()) {
-            App.showInfoMessage("Follow this user to view playlists","");
+            App.showInfoMessage("Follow this user to view playlists", "");
             return;
         }
         String query = """
@@ -266,7 +269,8 @@ public class selectedUser implements Initializable {
     }
 
 
-    @FXML private  void navigate (){
+    @FXML
+    private void navigate() {
         Helper.navigateUser(navigator);
     }
 
@@ -275,18 +279,63 @@ public class selectedUser implements Initializable {
         App.navigateTo("login");
     }
 
-    @FXML private void getPop() throws SQLException {
-        int category_id=1;
-        int user_id = getUserID(App.getUserEmail());
 
+    @FXML
+    private void getPop() throws SQLException {
+        int category_id = 1;
+        playlistCopy(category_id);
     }
-    @FXML private  void getJazz(){
-        int category_id=2;
+
+    @FXML
+    private void getJazz() throws SQLException {
+        int category_id = 2;
+        playlistCopy(category_id);
     }
-    @FXML private void getClassic(){
-        int category_id=3;
+
+    private void playlistCopy(int category_id) throws SQLException {
+        int follower_id = getUserID(App.getUserEmail());
+        String playlistIdquery = "select id from playlist where user_id = ? and category_id = ?";
+        PreparedStatement firstStatement = App.connection.prepareStatement(playlistIdquery);
+        firstStatement.setInt(1, follower_id);
+        firstStatement.setInt(2, category_id);
+        ResultSet rs = executeQuery(firstStatement);
+        rs.next();
+        int playlistId = rs.getInt("id");
+
+        String playlistMergeQuery = """
+                INSERT INTO playlist_song (playlist_id, song_id)
+                    (select ? , playlist_2.song_id
+                     from (select song_id
+                           from playlist_song
+                                    join playlist p on p.id = playlist_song.playlist_id
+                           where category_id = ?
+                             and user_id = ?) as playlist_1
+                              RIGHT JOIN (
+                         select song_id
+                         from playlist_song
+                                  join playlist p on p.id = playlist_song.playlist_id
+                         where category_id = ?
+                           and user_id = ?) as playlist_2
+                                         ON playlist_1.song_id = playlist_2.song_id
+                     WHERE playlist_1.song_id is null)
+                     """;
+        PreparedStatement insertStatement = App.connection.prepareStatement(playlistMergeQuery);
+        insertStatement.setInt(1, playlistId);
+        insertStatement.setInt(2, category_id);
+        insertStatement.setInt(3, follower_id);
+        insertStatement.setInt(4, category_id);
+        insertStatement.setInt(5, user_id);
+        execute(insertStatement);
     }
-    @FXML private void follow() throws SQLException {
+
+    @FXML
+    private void getClassic() throws SQLException {
+        int category_id = 3;
+        playlistCopy(category_id);
+    }
+
+    @FXML
+    private void follow() throws SQLException {
         String idQuery = "SELECT * FROM follower_following WHERE follower_id= ? AND following_id = ?";
 
         PreparedStatement idStatement = App.connection.prepareStatement(idQuery);
@@ -294,7 +343,7 @@ public class selectedUser implements Initializable {
         idStatement.setInt(2, user_id);
         ResultSet idResult = executeQuery(idStatement);
         if (idResult.next()) {
-            App.showError("Already following this user, ","");
+            App.showError("Already following this user, ", "");
             return;
         }
         String query = "INSERT INTO follower_following (follower_id, following_id) VALUES (?, ?)";
@@ -302,7 +351,7 @@ public class selectedUser implements Initializable {
         statement.setInt(1, getUserID(App.getUserEmail()));
         statement.setInt(2, user_id);
         execute(statement);
-        App.showSuccessMessage("Added to following","");
+        App.showSuccessMessage("Added to following", "");
         populateJazzChart();
         populateClassicChart();
         populatePopChart();
