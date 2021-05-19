62a747c3ff1e32b6a229b5a2b62864911d52ddc8
Ali Elbashir <alielbashiir@gmail.com>
2021-05-19 16:59:49 +0300

    Add genre charts

--- src/Home/Controllers/genreCharts.java
+++ src/Home/Controllers/genreCharts.java
@@ -2,16 +2,27 @@ package Home.Controllers;
 
 import Home.App;
 import Home.Helper;
+import Home.Modules.Song;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+
+import static Home.Helper.executeQuery;
 
 public class genreCharts implements Initializable {
     @FXML
@@ -23,10 +34,129 @@ public class genreCharts implements Initializable {
     @FXML
     ChoiceBox<String> navigator;
 
+    @FXML
+    TableView<Song> popList;
+    @FXML
+    private TableColumn<Song, Integer> popOrder;
+    @FXML
+    private TableColumn<Song, Integer> popName;
+
+    @FXML
+    private TableColumn<Song, Integer> popStreams;
+    
+
+    @FXML
+    TableView<Song> jazzList;
+    @FXML
+    private TableColumn<Song, Integer> jazzOrder;
+    @FXML
+    private TableColumn<Song, Integer> jazzName;
+
+    @FXML
+    private TableColumn<Song, Integer> jazzStreams;
+
+    @FXML
+    TableView<Song> classicList;
+    @FXML
+    private TableColumn<Song, Integer> classicOrder;
+    @FXML
+    private TableColumn<Song, Integer> classicName;
+
+    @FXML
+    private TableColumn<Song, Integer> classicStreams;
+    
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         Helper.getUserData(displayName, profilePicture);
         Helper.populateUserNavigator(navigator, "Charts");
+        populateCharts();
+    }
+
+    private void populateCharts() {
+        populatePopChart();
+        populateJazzChart();
+        populateClassicChart();
+    }
+
+    private void populateClassicChart() {
+        final ObservableList<Song> data = FXCollections.observableArrayList();
+        classicOrder.setCellValueFactory(new PropertyValueFactory<>("Order"));
+        classicName.setCellValueFactory(new PropertyValueFactory<>("Name"));
+        classicStreams.setCellValueFactory(new PropertyValueFactory<>("Streams"));
+        String query = """
+                select song.name as name, SUM(streams.streams) as streams
+                from song
+                         join streams on song.id = streams.song_id join album on album.id = song.album_id where album.category_id = ?
+                group by song.name ORDER BY streams DESC""";
+        try (PreparedStatement statement = App.connection.prepareStatement(query)) {
+            statement.setInt(1, 3);
+            ResultSet resultSet = executeQuery(statement);
+            int i = 1;
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                int streams = resultSet.getInt("streams");
+                data.add(new Song(i, name, streams));
+                i++;
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        classicList.setItems(data);
+    }
+
+    private void populateJazzChart() {
+        final ObservableList<Song> data = FXCollections.observableArrayList();
+        jazzOrder.setCellValueFactory(new PropertyValueFactory<>("Order"));
+        jazzName.setCellValueFactory(new PropertyValueFactory<>("Name"));
+        jazzStreams.setCellValueFactory(new PropertyValueFactory<>("Streams"));
+        String query = """
+                select song.name as name, SUM(streams.streams) as streams
+                from song
+                         join streams on song.id = streams.song_id join album on album.id = song.album_id where album.category_id = ?
+                group by song.name ORDER BY streams DESC""";
+        try (PreparedStatement statement = App.connection.prepareStatement(query)) {
+            statement.setInt(1, 2);
+            ResultSet resultSet = executeQuery(statement);
+            int i = 1;
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                int streams = resultSet.getInt("streams");
+                data.add(new Song(i, name, streams));
+                i++;
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        jazzList.setItems(data);
+    }
+
+    private void populatePopChart() {
+        final ObservableList<Song> data = FXCollections.observableArrayList();
+        popOrder.setCellValueFactory(new PropertyValueFactory<>("Order"));
+        popName.setCellValueFactory(new PropertyValueFactory<>("Name"));
+        popStreams.setCellValueFactory(new PropertyValueFactory<>("Streams"));
+        String query = """
+                select song.name as name, SUM(streams.streams) as streams
+                from song
+                         join streams on song.id = streams.song_id join album on album.id = song.album_id where album.category_id = ?
+                group by song.name ORDER BY streams DESC""";
+        try (PreparedStatement statement = App.connection.prepareStatement(query)) {
+            statement.setInt(1, 1);
+            ResultSet resultSet = executeQuery(statement);
+            int i = 1;
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                int streams = resultSet.getInt("streams");
+                data.add(new Song(i, name, streams));
+                i++;
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        popList.setItems(data);
     }
 
     @FXML
