7f3f4331c52e72e580fe7efe3d4645cd7f3002bb
Rammah Ali <nydoerd@gmail.com>
2021-05-19 09:52:03 +0300

    update songs section

--- src/Home/Controllers/songs.java
+++ src/Home/Controllers/songs.java
@@ -22,6 +22,7 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
 
+import static Home.Helper.execute;
 import static Home.Helper.executeQuery;
 
 public class songs implements Initializable {
@@ -67,6 +68,7 @@ public class songs implements Initializable {
 
     @FXML
     private TableColumn<Song, String> colReleaseDate;
+    ObservableList<String> songArtists = FXCollections.observableArrayList();
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         Helper.getUserData(displayName, profilePicture);
@@ -85,7 +87,7 @@ public class songs implements Initializable {
         colAlbum.setCellValueFactory(new PropertyValueFactory<>("album"));
         colLength.setCellValueFactory(new PropertyValueFactory<>("length"));
         colReleaseDate.setCellValueFactory(new PropertyValueFactory<>("releaseDate"));
-        String query = "SELECT * FROM song";
+        String query = "SELECT * FROM song ORDER BY id";
         try (PreparedStatement statement = App.connection.prepareStatement(query)) {
             ResultSet resultSet = executeQuery(statement);
             while (resultSet.next()) {
@@ -106,97 +108,107 @@ public class songs implements Initializable {
 
     @FXML
     private void createSong() throws SQLException {
-//        if (!name.getText().equals("") ) {
-//            int artistID = artist.getSelectionModel().getSelectedIndex()+1;
-//            int categoryID = category.getSelectionModel().getSelectedIndex()+1;
-//            if(releaseDate.getValue()==null){
-//                setCurrentDate(releaseDate);
-//            }
-//            String artistQuery = "SELECT * FROM album WHERE name = ?";
-//            PreparedStatement artistStatement = App.connection.prepareStatement(artistQuery);
-//            artistStatement.setString(1, name.getText());
-//            ResultSet artistResult = executeQuery(artistStatement);
-//            if(artistResult.next())
-//            {
-//                    App.showError("Album exists","An album with this name does exist ,please change the album's name");
-//                    return;
-//            }
-//            String query = "INSERT INTO album(name, artist_id, release_date, category_id, picture) VALUES (?, ?, ?, ?, ?)";
-//            PreparedStatement statement = App.connection.prepareStatement(query);
-//            statement.setString(1, name.getText());
-//            statement.setInt(2, artistID);
-//            statement.setDate(3, Date.valueOf(releaseDate.getValue()));
-//            statement.setInt(4, categoryID);
-//            statement.setString(5, picture.getText());
-//            execute(statement);
-//            importAlbums();
-//            App.showSuccessMessage("Album " + name.getText() + " has been created", "");
-//            clear();
-//        }
+        if (!name.getText().equals("") &&songArtists.size()!=0 ){
+
+            if(releaseDate.getValue()==null){
+                setCurrentDate(releaseDate);
+            }
+            String songQuery = "SELECT * FROM song WHERE name = ?";
+            PreparedStatement songStatement = App.connection.prepareStatement( songQuery);
+            songStatement.setString(1, name.getText());
+            ResultSet songResult = executeQuery(songStatement);
+            if(songResult.next())
+            {
+                    App.showError("Song exists"," a song with this name does exist ,please change the song's name");
+                    return;
+            }
+            int album_id = album.getSelectionModel().getSelectedIndex()+1;
+            int length;
+            if(this.length.getText().equals(""))
+                length=0;
+            else
+                length=Integer.parseInt(this.length.getText());
+            String query = "INSERT INTO song(name, album_id, length, release_date) VALUES (?, ?, ?, ?) ";
+            PreparedStatement statement = App.connection.prepareStatement(query);
+            statement.setString(1, name.getText());
+            statement.setInt(2, album_id);
+            statement.setInt(3, length);
+            statement.setDate(4, Date.valueOf(releaseDate.getValue()));
+            execute(statement);
+            setSongArtists(getSongByName(name.getText()));
+            importSongs();
+            App.showSuccessMessage("Song " + name.getText() + " has been created", "");
+            clear();
+        }
     }
 
     @FXML
     private void deleteSong() throws SQLException {
 
-//        if (!name.getText().equals("")) {
-//            String nameQuery = "SELECT * FROM album WHERE name = ?";
-//            PreparedStatement nameStatement = App.connection.prepareStatement(nameQuery);
-//            nameStatement.setString(1, name.getText());
-//            ResultSet nameResult = executeQuery(nameStatement);
-//            if(!nameResult.next())
-//            {
-//                App.showError("Album doesn't exists","no such album with this name");
-//                return;
-//            }
-//            Album album =  albumsTable.getSelectionModel().getSelectedItem();
-//            int id = album.getID();
-//            String query = "DELETE FROM  album WHERE id = ?";
-//            PreparedStatement statement = App.connection.prepareStatement(query);
-//            statement.setInt(1, id);
-//            execute(statement);
-//            App.showSuccessMessage("Album " + name.getText() + " has been deleted", "");
-//            importAlbums();
-//            clear();
-//        }
+        if (!name.getText().equals("")) {
+            String nameQuery = "SELECT * FROM song WHERE name = ?";
+            PreparedStatement nameStatement = App.connection.prepareStatement(nameQuery);
+            nameStatement.setString(1, name.getText());
+            ResultSet nameResult = executeQuery(nameStatement);
+            if(!nameResult.next())
+            {
+                App.showError("Song doesn't exists","no such song with this name");
+                return;
+            }
+            Song song =  songsTable.getSelectionModel().getSelectedItem();
+            int id = song.getID();
+            String query = "DELETE FROM  song WHERE id = ?";
+            PreparedStatement statement = App.connection.prepareStatement(query);
+            statement.setInt(1, id);
+            execute(statement);
+            App.showSuccessMessage("Song " + name.getText() + " has been deleted", "");
+            importSongs();
+            clear();
+        }
     }
+
 
 
     @FXML
     private void updateSong() throws SQLException {
-//        if (!name.getText().equals("")) {
-//            int artistID = artist.getSelectionModel().getSelectedIndex()+1;
-//            int categoryID = category.getSelectionModel().getSelectedIndex()+1;
-//            Album album = albumsTable.getSelectionModel().getSelectedItem();
-//            int id;
-//            if(album==null)
-//                id=-1;
-//            else
-//                id=album.getID();
-//            if(releaseDate.getValue()==null){
-//                setCurrentDate(releaseDate);
-//            }
-//            String query = "UPDATE  album SET name =?,artist_id =?,release_date =?,category_id =?,picture = ? WHERE id = ?";
-//            PreparedStatement statement = App.connection.prepareStatement(query);
-//            statement.setString(1, name.getText());
-//            statement.setInt(2, artistID);
-//            statement.setDate(3, Date.valueOf(releaseDate.getValue()));
-//            statement.setInt(4, categoryID);
-//            statement.setString(5, picture.getText());
-//            statement.setInt(6, id);
-//
-//            if (execute(statement) != 0) {
-//                importAlbums();
-//                App.showSuccessMessage("Album " + name.getText() + " has been updated", "");
-//            } else {
-//                App.showError("Album does not exist", "");
-//            }
-//            clear();
-//
-//        }
+        if (!name.getText().equals("")) {
+            int album_id = album.getSelectionModel().getSelectedIndex()+1;
+            int length;
+            if(this.length.getText().equals(""))
+                length=0;
+            else
+                length=Integer.parseInt(this.length.getText());
+            Song song = songsTable.getSelectionModel().getSelectedItem();
+            int id;
+            if(song==null)
+                id=-1;
+            else
+                id=song.getID();
+            if(releaseDate.getValue()==null){
+                setCurrentDate(releaseDate);
+            }
+            String query = "UPDATE song SET name =?,album_id =?,length =?,release_date=? WHERE id = ?";
+            PreparedStatement statement = App.connection.prepareStatement(query);
+            statement.setString(1, name.getText());
+            statement.setInt(2, album_id);
+            statement.setInt(3, length);
+            statement.setDate(4, Date.valueOf(releaseDate.getValue()));
+            statement.setInt(5, id);
+
+            if (execute(statement) != 0) {
+                importSongs();
+                updateSongArtist(song.getID());
+                App.showSuccessMessage("Song " + name.getText() + " has been updated", "");
+            } else {
+                App.showError("Song does not exist", "");
+            }
+            clear();
+
+        }
     }
 
     @FXML
-    private void onRowClickAction() throws SQLException {
+    private void onRowClickAction()  {
         Song song = songsTable.getSelectionModel().getSelectedItem();
         if (song != null) {
             name.setText(song.getName());
@@ -207,12 +219,38 @@ public class songs implements Initializable {
         }
     }
 
+    @FXML  private  void  addArtist(){
+        String artist = fullArtistList.getSelectionModel().getSelectedItem();
+        if(artist!=null){
+            if(songArtists.contains(artist)){
+                App.showInfoMessage("This Artist is already added","");
+                return;
+            }
+            songArtists.add(artist);
+            songArtistlist.setItems(songArtists);
+           songArtistlist.getSelectionModel().select(songArtists.size()-1);
+        }
+    }
+    @FXML private  void  deleteArtist(){
+        String artist = songArtistlist.getSelectionModel().getSelectedItem();
+        if(artist!=null){
+            int selectedIndex = songArtistlist.getSelectionModel().getSelectedIndex();
+            songArtists.remove(artist);
+            songArtistlist.setItems(songArtists);
+            selectedIndex=selectedIndex-1;
+           if(selectedIndex!=0){
+               songArtistlist.getSelectionModel().select(selectedIndex-1);
+           }
+        }
+    }
+
+
     private void clear() {
-//        name.setText("");
-//        picture.setText("");
-//       releaseDate.setValue(null);
-//       artist.getSelectionModel().select(0);
-//       category.getSelectionModel().select(0);
+        name.setText("");
+       releaseDate.setValue(null);
+       album.getSelectionModel().select(0);
+       length.setText("");
+       songArtists.clear();
     }
 
 
@@ -264,6 +302,7 @@ public class songs implements Initializable {
             while (resultSet.next()) {
                 String artist_name = resultSet.getString("name");
                 songArtists.add(artist_name);
+                this.songArtists.add(artist_name);
             }
             songArtistlist.setItems(songArtists);
             songArtistlist.getSelectionModel().select(0);
@@ -272,6 +311,29 @@ public class songs implements Initializable {
         }
     }
 
+    private void setSongArtists(int songID) throws SQLException {
+       for(int i =0;i<songArtists.size();i++){
+           int artist_id = getArtistByName(songArtists.get(i));
+           String query = "INSERT INTO song_artist(song_id, artist_id) VALUES (?, ?)";
+           PreparedStatement statement = App.connection.prepareStatement(query);
+           statement.setInt(1, songID);
+           statement.setInt(2, artist_id);
+           execute(statement);
+       }
+    }
+
+    private void updateSongArtist(int id) throws SQLException {
+          deleteSongArtists(id);
+          setSongArtists(id);
+    }
+
+    @FXML
+    private void deleteSongArtists(int id) throws SQLException {
+            String query = "DELETE FROM  song_artist WHERE song_id = ?";
+            PreparedStatement statement = App.connection.prepareStatement(query);
+            statement.setInt(1, id);
+            execute(statement);
+        }
 
     private String getAlbum(int id) throws SQLException {
         String query = "SELECT * FROM album WHERE id = ?";
@@ -282,10 +344,25 @@ public class songs implements Initializable {
         return resultSet.getString("name");
     }
 
+    private int getArtistByName(String name) throws SQLException {
+        String query = "SELECT * FROM artist WHERE name = ?";
+        PreparedStatement statement = App.connection.prepareStatement(query);
+        statement.setString(1, name);
+        ResultSet resultSet = executeQuery(statement);
+        resultSet.next();
+        return resultSet.getInt("id");
+    }
 
+    private int getSongByName(String name) throws SQLException {
+        String query = "SELECT * FROM song WHERE name = ?";
+        PreparedStatement statement = App.connection.prepareStatement(query);
+        statement.setString(1, name);
+        ResultSet resultSet = executeQuery(statement);
+        resultSet.next();
+        return resultSet.getInt("id");
+    }
     private void setCurrentDate(DatePicker datePicker){
         java.util.Date input = new java.util.Date();
-//        LocalDate date = input.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
         LocalDate date = LocalDate.parse(new SimpleDateFormat("yyyy-MM-dd").format(input));
        datePicker.setValue(date);
     }
@@ -304,7 +381,7 @@ public class songs implements Initializable {
         ObservableList<String> pages = FXCollections.observableArrayList();
         pages.addAll("Dashboard", "Users", "Artists", "Albums", "Songs");
         navigator.setItems(pages);
-        navigator.getSelectionModel().select(3);
+        navigator.getSelectionModel().select(4);
     }
 
     @FXML
